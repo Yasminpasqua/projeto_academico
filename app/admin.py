67ef5849a2e569274629510f62164d64a80ff6c0
Yasmin Pereira Pasqua <13771997623@MUZAMBINHO.EDU.BR>
2024-11-19 16:52:31 -0300

    AAAAAAAAAAAA

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(pessoaa)
 admin.site.register(Disciplina)
+admin.site.register(Cidade)
+admin.site.register(Turma)
+admin.site.register(Area_do_saber)
+admin.site.register(Curso)
+admin.site.register(Turno)
+admin.site.register(Tipo_avaliacao)
+admin.site.register(Ocorrencia)
+admin.site.register(Instituicao)
+admin.site.register(Manter_disciplinas)
+admin.site.register(Frequencia)
+admin.site.register(Avaliacao)
+
+class pessoaaInline(admin.TabularInline):
+    model = pessoaa
+    extra = 1 
+
+class Ocupacao_pessoaAdmin(admin.ModelAdmin):
+
+    inlines = [pessoaaInline]
+
+admin.site.register(pessoaa)
+admin.site.register(Ocupacao_pessoa, Ocupacao_pessoaAdmin)
+
+
+
